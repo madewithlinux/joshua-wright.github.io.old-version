4a5f9f62c522c0f8033127afbfe1286ed626617c
madewithlinux <madewithlinux.josh@gmail.com>
2016-02-18 22:25:58 -0600

    added more words; functionality for words not found

--- alphabet.js
+++ alphabet.js
@@ -2,66 +2,90 @@
  * Created by j0sh on 2/14/16.
  */
 
-chars = {
-    "A": "<tr><td>A</td><td>Alpha</td><td>Apple</td></tr>",
-    "B": "<tr><td>B</td><td>Bravo</td><td>Boy</td></tr>",
-    "C": "<tr><td>C</td><td>Charlie</td><td></td></tr>",
-    "D": "<tr><td>D</td><td>Delta</td><td></td></tr>",
-    "E": "<tr><td>E</td><td>Echo</td><td></td></tr>",
-    "F": "<tr><td>F</td><td>Foxtrot</td><td>Fire</td></tr>",
-    "G": "<tr><td>G</td><td>Golf</td><td></td></tr>",
-    "H": "<tr><td>H</td><td>Hotel</td><td>Horse</td></tr>",
-    "I": "<tr><td>I</td><td>India</td><td></td></tr>",
-    "J": "<tr><td>J</td><td>Juliett</td><td></td></tr>",
-    "K": "<tr><td>K</td><td>Kilo</td><td>King</td></tr>",
-    "L": "<tr><td>L</td><td>Lima</td><td>Lemon</td></tr>",
-    "M": "<tr><td>M</td><td>Mike</td><td></td></tr>",
-    "N": "<tr><td>N</td><td>November</td><td></td></tr>",
-    "O": "<tr><td>O</td><td>Oscar</td><td>Orange</td></tr>",
-    "P": "<tr><td>P</td><td>Papa</td><td></td></tr>",
-    "Q": "<tr><td>Q</td><td>Quibec</td><td>Quarter</td></tr>",
-    "R": "<tr><td>R</td><td>Romeo</td><td></td></tr>",
-    "S": "<tr><td>S</td><td>Sierra</td><td>Snake</td></tr>",
-    "T": "<tr><td>T</td><td>Tango</td><td>Terrific</td></tr>",
-    "U": "<tr><td>U</td><td>Uniform</td><td></td></tr>",
-    "V": "<tr><td>V</td><td>Victor</td><td></td></tr>",
-    "W": "<tr><td>W</td><td>Whiskey</td><td>Wallrus</td></tr>",
-    "X": "<tr><td>X</td><td>X-ray</td><td></td></tr>",
-    "Y": "<tr><td>Y</td><td>Yankee</td><td></td></tr>",
-    "Z": "<tr><td>Z</td><td>Zulu</td><td>Zebra</td></tr>",
-    "0": "<tr><td>0</td><td>Zero</td><td></td></tr>",
-    "1": "<tr><td>1</td><td>One</td><td></td></tr>",
-    "2": "<tr><td>2</td><td>Two</td><td></td></tr>",
-    "3": "<tr><td>3</td><td>Three</td><td></td></tr>",
-    "4": "<tr><td>4</td><td>Four</td><td></td></tr>",
-    "5": "<tr><td>5</td><td>Five</td><td></td></tr>",
-    "6": "<tr><td>6</td><td>Six</td><td></td></tr>",
-    "7": "<tr><td>7</td><td>Seven</td><td></td></tr>",
-    "8": "<tr><td>8</td><td>Eight</td><td></td></tr>",
-    "9": "<tr><td>9</td><td>Nine</td><td></td></tr>",
-    ".": "<tr><td>.</td><td>Dot</td><td>Period</td></tr>"
+var chars = {
+    "A": ["A", "Alpha", "Apple"],
+    "B": ["B", "Bravo", "Boy"],
+    "C": ["C", "Charlie", ""],
+    "D": ["D", "Delta", ""],
+    "E": ["E", "Echo", ""],
+    "F": ["F", "Foxtrot", "Fire"],
+    "G": ["G", "Golf", ""],
+    "H": ["H", "Hotel", "Horse"],
+    "I": ["I", "India", ""],
+    "J": ["J", "Juliett", ""],
+    "K": ["K", "Kilo", "King"],
+    "L": ["L", "Lima", "Lemon"],
+    "M": ["M", "Mike", ""],
+    "N": ["N", "November", ""],
+    "O": ["O", "Oscar", "Orange"],
+    "P": ["P", "Papa", ""],
+    "Q": ["Q", "Quibec", "Quarter"],
+    "R": ["R", "Romeo", ""],
+    "S": ["S", "Sierra", "Snake"],
+    "T": ["T", "Tango", "Terrific"],
+    "U": ["U", "Uniform", ""],
+    "V": ["V", "Victor", ""],
+    "W": ["W", "Whiskey", "Wallrus"],
+    "X": ["X", "X-ray", ""],
+    "Y": ["Y", "Yankee", ""],
+    "Z": ["Z", "Zulu", "Zebra"],
+    "0": ["0", "Zero", ""],
+    "1": ["1", "One", ""],
+    "2": ["2", "Two", ""],
+    "3": ["3", "Three", ""],
+    "4": ["4", "Four", ""],
+    "5": ["5", "Five", ""],
+    "6": ["6", "Six", ""],
+    "7": ["7", "Seven", ""],
+    "8": ["8", "Eight", ""],
+    "9": ["9", "Nine", ""],
+    ".": [".", "Dot", "Period"],
+    "-": ["-", "Dash", "Hyphen"],
+    "_": ["-", "Underscore", ""],
+    ":": [":", "Colon", ""],
+    ";": ["$", "Dollar Sign", ""],
+    "!": ["!", "Exclamation Mark", ""],
+    "@": ["@", "At Sign", ""],
+    "$": ["", "Semi Colon", ""],
+    "%": ["%", "Percentage Sign", ""],
+    "^": ["^", "Caret", ""],
+    "&": ["&", "Amperstand", "And Symbol"],
+    "*": ["*", "Asterisk", ""],
+    "/": ["/", "Forward Slash", ""],
+    "\\": ["\\", "BackSlash", ""],
+    "\n": ["\\n", "New LineReturn", ""],
+    "\t": ["\\t", "Tab", ""],
+    " ": [" ", "Space", ""]
 };
 
-function spell_input() {
-    var phrase = $("#phrase_input").val();
-    var output = $("#output_table")[0];
-    output.innerHTML = "";
-    for (var i = 0; i < phrase.length; i++) {
-        if (phrase[i] == ' ') {
-            /*skip spaces*/
-            continue;
-        }
-        output.innerHTML += chars[phrase[i].toUpperCase()];
-    }
-}
 $(document).ready(function () {
     var input = $("#phrase_input");
+    var output = $("#output_table")[0];
+
+    function spell_input() {
+        output.innerHTML = "";
+        var phrase = input.val();
+        for (var i = 0; i < phrase.length; i++) {
+            if (chars.hasOwnProperty(phrase[i].toUpperCase())) {
+                /*print the stuff if we have a word for this character*/
+                output_table.innerHTML += "<tr><td>" + chars[phrase[i].toUpperCase()][0] +
+                    "</td><td>" + chars[phrase[i].toUpperCase()][1] +
+                    "</td><td>" + chars[phrase[i].toUpperCase()][2] + "</td></tr>";
+            } else {
+                /*fallback on just displaying the character*/
+                output_table.innerHTML += "<tr><td>" + phrase[i].toUpperCase() + "</td><td></td><td></td></tr>";
+            }
+        }
+    }
+
     input.keyup(function () {
         /*re-run whenever input changes*/
         spell_input();
     });
 
     /*always be focused*/
-    setInterval(function(){input.focus()}, 100);
+    setInterval(function () {
+        input.focus()
+    }, 100);
 
 });
