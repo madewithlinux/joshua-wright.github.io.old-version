7ce8e7367039651a1844bca3e6864565063cf893
madewithlinux <madewithlinux.josh@gmail.com>
2016-02-28 17:18:57 -0600

    saves values to cookie and has remove button

--- web_code/resistor.js
+++ web_code/resistor.js
@@ -15,25 +15,23 @@ var colors = {
     8: "Gray",
     9: "White"
 };
+var storage_cookie = "resistor_values";
 
 $("document").ready(function () {
 
     /*jQueries defined outside functions for caching*/
     var output_table = $("#output")[0];
     var input = $("#value");
-    /*default values for when the page loads*/
-    /*store these in an object for automatic duplicate resolution and sorting*/
-    var values = {
-        1: {},
-        5: {},
-        10: {},
-        100: {},
-        1000: {},
-        2000: {},
-        3300: {},
-        5100: {},
-        10000: {}
-    };
+    var values;
+    if (Cookies.get(storage_cookie) == undefined) {
+        /*no cookie to load from, so set defaults*/
+        values = new Set([1, 5, 10, 100, 1000, 2000, 3300, 5100, 10000]);
+        Cookies.set(storage_cookie, [1, 5, 10, 100, 1000, 2000, 3300, 5100, 10000]);
+    } else {
+        /*load from the cookie*/
+        /*Using eval(): unsafe? Maybe. Works? Yes.*/
+        values = new Set(eval(Cookies.get(storage_cookie)));
+    }
 
     function write_value(value) {
         /*preserve the original value for printing later*/
@@ -45,15 +43,17 @@ $("document").ready(function () {
             value /= 10;
             third++;
         }
+        var first;
+        var second;
         if (third == 0) {
             /*case where multiplier is 0, therefore value is direct*/
-            var first = 0;
-            var second = Math.round(value);
+            first = 0;
+            second = Math.round(value);
         } else {
             /*case where multiplier is non-zero, therefore first and second make a decimal*/
             third--;
-            var first = Math.floor(value);
-            var second = Math.round((10 * value) % 10);
+            first = Math.floor(value);
+            second = Math.round((10 * value) % 10);
         }
         /*simply append a row to the table*/
         output_table.innerHTML += "<tr>" +
@@ -62,24 +62,55 @@ $("document").ready(function () {
             "<td style='background: " + colors[first] + ";'>" + colors[first] + "</td>" +
             "<td style='background: " + colors[second] + ";'>" + colors[second] + "</td>" +
             "<td style='background: " + colors[third] + ";'>" + colors[third] + "</td>" +
+                /*store the unformatted number in the id attribute*/
+            "<td class='remove_btn' id='" + Number(original_value).toString() + "'> <strong><a href='#'>X</a></strong></td>" +
             "</tr>";
+    }
+
+    function save_values() {
+        var values_tmp = [];
+        values.forEach(function (v, i) {
+            values_tmp.push(v);
+        });
+        /*save resistor values*/
+        Cookies.set(storage_cookie, values_tmp);
     }
 
     function write_values() {
         output_table.innerHTML = "";
-        $.each(values, function (v, i) {
+        var values_tmp = [];
+        values.forEach(function (v, i) {
+            values_tmp.push(v);
+        });
+        values_tmp.sort(function (a, b) {
+            /*use subtraction, not comparison operators, because sort needs
+             * three-output comparison*/
+            return a - b;
+        });
+        values_tmp.forEach(function (v, i) {
             write_value(v);
         });
+        console.log(values_tmp);
+        a = values_tmp;
+        /*set callbacks for remove buttons*/
+        /*re-run the jQuery evey time, because the page layout will has changed*/
+        $(".remove_btn").click(function () {
+            values.delete(Number(this.id));
+            this.parentNode.remove();
+            save_values();
+        });
+        save_values();
     }
 
     function write_new() {
+        /*add the new number*/
+        values.add(Number(input.val()));
         /*re-generate the whole table*/
-        values[Number(input.val())] = {};
         write_values();
     }
 
     input.keyup(function (event) {
-        if (event.keyCode == 13) {
+        if (event.keyCode == 13/*enter key*/) {
             write_new();
         }
     });
@@ -90,7 +121,6 @@ $("document").ready(function () {
 
     /*generate initial values for when the page loads*/
     write_values();
-
 
 })
 ;
