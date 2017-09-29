25473dba96fcbe2afac08a1317b0c3384a485ec7
Josh Wright <madewithlinux.josh@gmail.com>
2017-09-29 17:56:10 -0500

    use es6 syntax

--- js/iat.js
+++ js/iat.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var source_hangman = "[\n" +
+    const source_hangman = "[\n" +
         "    [\n" +
         "        \"image\",\n" +
         "        [ [-1,1],  [0,1] ],\n" +
@@ -22,7 +22,7 @@
         "    ]\n" +
         "]";
 
-    var source_sierpinski = "[\n" +
+    const source_sierpinski = "[\n" +
         "    [\n" +
         "        [0.5, 0.0, -0.5],\n" +
         "        [0.0, 0.5, -0.5]\n" +
@@ -37,7 +37,7 @@
         "    ]\n" +
         "]";
 
-    var source_l_shape = "[\n" +
+    const source_l_shape = "[\n" +
         "    [\n" +
         "        \"image\",\n" +
         "        [ [ 1, 1], [ 0.25, 0.25] ],\n" +
@@ -64,7 +64,7 @@
         "    ]\n" +
         "]";
 
-    var fractal_sources = {
+    const fractal_sources = {
         sierpinski: source_sierpinski,
         hangman: source_hangman,
         l_shape: source_l_shape,
@@ -73,59 +73,59 @@
     function image(px, py, p2x, p2y, qx, qy, q2x, q2y, rx, ry, r2x, r2y) {
 
         // efficient matrix inversion code (math from wikipedia)
-        var a = px;
-        var b = qx;
-        var c = rx;
-        var d = py;
-        var e = qy;
-        var f = ry;
-        var g = 1;
-        var h = 1;
-        var i = 1;
+        const a = px;
+        const b = qx;
+        const c = rx;
+        const d = py;
+        const e = qy;
+        const f = ry;
+        const g = 1;
+        const h = 1;
+        const i = 1;
 
-        var A = +(e * i - f * h);
-        var B = -(d * i - f * g);
-        var C = +(d * h - e * g);
-        var D = -(b * i - c * h);
-        var E = +(a * i - c * g);
-        var F = -(a * h - b * g);
-        var G = +(b * f - c * e);
-        var H = -(a * f - c * d);
-        var I = +(a * e - b * d);
+        const A = +(e * i - f * h);
+        const B = -(d * i - f * g);
+        const C = +(d * h - e * g);
+        const D = -(b * i - c * h);
+        const E = +(a * i - c * g);
+        const F = -(a * h - b * g);
+        const G = +(b * f - c * e);
+        const H = -(a * f - c * d);
+        const I = +(a * e - b * d);
 
-        var det = a * A + b * B + c * C;
-        var det_inv = 1 / det;
+        const det = a * A + b * B + c * C;
+        const det_inv = 1 / det;
 
-        var A_inv = [
+        const A_inv = [
             [A, D, G],
             [B, E, H],
             [C, F, I]
         ];
 
-        for (var i = 0; i < 3; ++i) {
-            for (var j = 0; j < 3; ++j) {
+        for (let i = 0; i < 3; ++i) {
+            for (let j = 0; j < 3; ++j) {
                 A_inv[i][j] *= det_inv;
             }
         }
 
         // matrix of output points
-        var out_pts = [
+        const out_pts = [
             [p2x, q2x, r2x],
             [p2y, q2y, r2y],
             [1, 1, 1]
         ];
 
-        var out = [
+        const out = [
             [0, 0, 0],
             [0, 0, 0],
             [0, 0, 0]
         ];
 
         // now multiply output point matrix by inverse of input matrix
-        for (var i = 0; i < 3; ++i) {
-            for (var j = 0; j < 3; ++j) {
-                var sum = 0;
-                for (var k = 0; k < 3; ++k) {
+        for (let i = 0; i < 3; ++i) {
+            for (let j = 0; j < 3; ++j) {
+                let sum = 0;
+                for (let k = 0; k < 3; ++k) {
                     sum += out_pts[i][k] * A_inv[k][j];
                 }
                 out[i][j] = sum;
@@ -135,9 +135,9 @@
     }
 
     function calculate_transforms(transforms) {
-        var out = [];
-        for (var i = 0; i < transforms.length; i++) {
-            var t = transforms[i];
+        let out = [];
+        for (let i = 0; i < transforms.length; i++) {
+            const t = transforms[i];
 
             if (Array.isArray(t) && typeof(t[0][0]) === 'number') {
                 out.push(t);
@@ -152,27 +152,27 @@
         return out;
     }
 
-    var input_textarea = document.getElementById("transforms");
+    const input_textarea = document.getElementById("transforms");
     input_textarea.value = source_sierpinski;
-    var go_button = document.getElementById("btn_render");
-    var canvas = document.getElementById("main_canvas");
-    var res_x = document.getElementById('res_x');
-    var res_y = document.getElementById('res_y');
-    var render_delay = document.getElementById('render_delay');
-    var max_points = document.getElementById('max_points');
+    const go_button = document.getElementById("btn_render");
+    const canvas = document.getElementById("main_canvas");
+    const res_x = document.getElementById('res_x');
+    const res_y = document.getElementById('res_y');
+    const render_delay = document.getElementById('render_delay');
+    const max_points = document.getElementById('max_points');
 
     function array_transform(matrices) {
-        var code = "";
+        let code = "";
         code += "(function (points, out){\n";
 
-        code += "for (var i=0; i < points.length/2; ++i) {\n";
+        code += "for (let i=0; i < points.length/2; ++i) {\n";
 
         // helper function to eliminate empty subexpressions
         function calculate_row(ax, ay, c) {
             ax = +ax;
             ay = +ay;
             c = +c;
-            var code = "";
+            let code = "";
 
             if (ax !== 0 && ay !== 0) {
                 code = "points[2*i  ] * " + ax + " + points[2*i+1] * " + ay;
@@ -195,8 +195,8 @@
         }
 
         // expand matrices
-        for (var idx = 0; idx < matrices.length; ++idx) {
-            var m = matrices[idx];
+        for (let idx = 0; idx < matrices.length; ++idx) {
+            const m = matrices[idx];
             code += "out[" + (2 * matrices.length) + "*i + " + (2 * idx + 0) + "] = ";
             code += calculate_row(m[0][0], m[0][1], m[0][2]);
             code += ";\n";
@@ -213,31 +213,32 @@
     }
 
     function display_points(points) {
-        // setup canvas
-        var width = canvas.width | 0;
-        var height = canvas.height | 0;
+        let i;
+// setup canvas
+        const width = canvas.width | 0;
+        const height = canvas.height | 0;
         console.log(width, height);
-        var ctx = canvas.getContext("2d");
+        const ctx = canvas.getContext("2d");
         ctx.fillStyle = 'rgb(0,0,0)';
         ctx.rect(0, 0, width, height);
         ctx.fill();
 
 
-        var image_data = ctx.createImageData(width, height);
+        const image_data = ctx.createImageData(width, height);
         // start with black background
-        for (var i = 0; i < image_data.data.length; i += 4) {
+        for (i = 0; i < image_data.data.length; i += 4) {
             image_data.data[i] = 0;
             image_data.data[i + 1] = 0;
             image_data.data[i + 2] = 0;
             image_data.data[i + 3] = 255;
         }
         // then add points
-        for (var i = 0; i < points.length / 2; ++i) {
-            var point_x = points[i * 2];
-            var point_y = points[i * 2 + 1];
-            var x = (point_x * width / 2 + width / 2) | 0;
-            var y = height - (point_y * height / 2 + height / 2) | 0;
-            var offset = 4 * (y * width + x);
+        for (i = 0; i < points.length / 2; ++i) {
+            const point_x = points[i * 2];
+            const point_y = points[i * 2 + 1];
+            const x = (point_x * width / 2 + width / 2) | 0;
+            const y = height - (point_y * height / 2 + height / 2) | 0;
+            const offset = 4 * (y * width + x);
             image_data.data[offset] = 255;
             image_data.data[offset + 1] = 255;
             image_data.data[offset + 2] = 255;
@@ -249,15 +250,15 @@
     function set_transforms(transforms) {
 
         console.log("build function");
-        var transform_source = array_transform(transforms);
+        const transform_source = array_transform(transforms);
         // console.log(transform_source);
-        var transformer = eval(transform_source);
+        const transformer = eval(transform_source);
 
         // start with two random points
-        var points = new Float64Array([Math.random() * 2 - 1, Math.random() * 2 - 1]);
+        let points = new Float64Array([Math.random() * 2 - 1, Math.random() * 2 - 1]);
 
         while (points.length < 10) {
-            var new_points = new Float64Array(transforms.length * points.length);
+            const new_points = new Float64Array(transforms.length * points.length);
             transformer(points, new_points);
             points = new_points;
         }
@@ -267,7 +268,7 @@
         // calculate rest of points async
         function continue_render() {
             if (points.length < max_points.value) {
-                var new_points = new Float64Array(transforms.length * points.length);
+                const new_points = new Float64Array(transforms.length * points.length);
                 transformer(points, new_points);
                 points = new_points;
                 display_points(points);
@@ -280,8 +281,8 @@
     }
 
     function render_fractal() {
-        var transforms = JSON.parse(input_textarea.value);
-        transforms = calculate_transforms(transforms);
+        const transforms_ = JSON.parse(input_textarea.value);
+        const transforms = calculate_transforms(transforms_);
         canvas.width = res_x.value;
         canvas.height = res_y.value;
         set_transforms(transforms);
@@ -296,9 +297,12 @@
         render_fractal();
     }
 
-    var buttons = document.getElementsByClassName('fractal-btn');
-    for (var i = 0; i < buttons.length; i++) {
-        buttons[i].onclick = pre_defined_fractal_btn;
+    const buttons = document.getElementsByClassName('fractal-btn');
+    for (const btn of buttons) {
+        btn.onclick = pre_defined_fractal_btn;
     }
+    // for (var i = 0; i < buttons.length; i++) {
+    //     buttons[i].onclick = pre_defined_fractal_btn;
+    // }
 
 })();
